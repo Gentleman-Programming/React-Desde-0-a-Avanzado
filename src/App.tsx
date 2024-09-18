7d36a4ee7b2368a0c0b1de031fa9c12ba60fe97a
alan buscaglia <alanbuscaglia@gmail.com>
2024-09-18 20:05:32 +0200

    feat: clase 06 useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,44 @@
-import { useState } from 'react'
-import { Button } from './components'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState("")
 
-  // batching
+  const fetchData = async () => {
+    setLoading(true)
+    try {
+      const response = await fetch("https://api.example.com/data")
 
-  const countMore = () => {
-    setCount((count) => count + 1)
-    setCount((count) => count + 1)
-    setCount((count) => count + 1)
-    setCount((count) => count + 1)
-    setCount((count) => count + 1)
-    setCount((count) => count + 1)
-    setCount((count) => count + 1)
-    setCount((count) => count + 1)
-    setCount((count) => count + 1)
+      if (!response.ok) {
+        throw new Error("Error al obtener datos")
+      }
+
+      const jsonData = await response.json()
+
+      setData(jsonData)
+    } catch (err) {
+      setError(err as string)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  if (loading) {
+    return <div>Cargando...</div>
+  }
+
+  if (error) {
+    return <div>UPS! Hay un error: {error}</div>
   }
 
   return (
-    <>
-      <Button label={`Count is ${count}`} parentMethod={countMore} />
-    </>
+    <div>{JSON.stringify(data)}</div>
   )
 }
 
