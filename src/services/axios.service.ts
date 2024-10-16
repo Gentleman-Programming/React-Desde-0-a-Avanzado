3e44681edc863733103f7721b5399be8f5b0f809
alan buscaglia <alanbuscaglia@gmail.com>
2024-10-16 20:31:38 +0200

    fix: remove bad import

--- src/services/axios.service.ts
+++ src/services/axios.service.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosInstance, AxiosRequestHeaders, AxiosResponse, InternalAxiosRequestConfig } from "axios";
+import axios, { AxiosInstance, AxiosResponse, InternalAxiosRequestConfig } from "axios";
 
 let axiosInstance: AxiosInstance;
 
